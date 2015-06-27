79d583295aab704a5b346af7537532a8698bee51
Joe Guilmette <joe.guilmette@gmail.com>
2015-06-27 17:41:49 +0700

    hello

---
+++ provision/install-rvm.sh
@@ -0,0 +1,7 @@
+ #!/usr/bin/env bash
+
+curl -#LO https://rvm.io/mpapis.asc
+
+gpg --import mpapis.asc
+
+curl -sSL https://get.rvm.io | bash -s $1
---
+++ provision/provision-pre.sh
@@ -0,0 +1,72 @@
+# Rubygems update
+
+if [ $(gem -v|grep '^2.') ]; then
+
+	echo "gem installed"
+
+else
+
+	apt-get install -y ruby-dev
+
+	echo "ruby-dev installed"
+
+	echo "gem not installed"
+
+	gem install rubygems-update
+
+	update_rubygems
+
+fi
+
+# use ruby 2.0.0 for photocopier
+rvm --default use 2.0.0
+
+# photocopier install
+# once 1.0.0 is released this will be unnecessary
+photocopier_install="$(gem list photocopier -i)"
+
+if [ "$photocopier_install" = true ]; then
+
+	echo "photocopier installed"
+
+else
+
+	echo "photocopier not installed"
+
+	gem install specific_install
+
+	gem specific_install -l	https://github.com/welaika/photocopier.git
+
+fi
+
+# wordmove install
+wordmove_install="$(gem list wordmove -i)"
+
+if [ "$wordmove_install" = true ]; then
+
+	echo "wordmove installed"
+
+else
+
+	echo "wordmove not installed"
+
+	gem install wordmove
+
+	wordmove_path="$(gem which wordmove | sed -s 's/.rb/\/deployer\/base.rb/')"
+
+	if [ "$(grep yaml $wordmove_path)" ]; then
+
+		echo "can require yaml"
+
+	else
+
+		echo "can't require yaml"
+
+		echo "set require yaml"
+
+		sed -i "7i require\ \'yaml\'" $wordmove_path
+
+		echo "can require yaml"
+
+	fi
+fi
